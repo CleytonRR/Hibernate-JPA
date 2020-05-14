a34de1b3e4ad5e5dc9d59fcd3574f5c54776c20b
cleytonRR <cleytonrodrigues69@live.com>
2020-05-13 21:33:21 -0300

    Method for create datas in database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class Main {
+
+    public static void main(String[] args) {
+        EntityManagerFactory entityManager = Persistence
+                .createEntityManagerFactory("AlgaWorks-PU");
+        entityManager.close();
+    }
 }
